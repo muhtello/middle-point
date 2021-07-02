d0435f7880ea218576e804a1f416fdee80544a58
muhanad212 <muhtello@gmail.com>
2021-07-02 11:27:43 +0200

    inti basic security

---
+++ src/main/java/com/example/middlepoint/config/security/PasswordEncoder.java
@@ -0,0 +1,12 @@
+package com.example.middlepoint.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+
+   @Bean
+   public BCryptPasswordEncoder bCryptPasswordEncoder (){
+      return new BCryptPasswordEncoder();
+   }
+}
